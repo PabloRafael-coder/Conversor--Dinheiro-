a00b581db28fb38c6cb3b487db20b7d1a697ff99
PabloRafael-coder <pabloofael@gmail.com>
2023-10-21 22:57:04 -0300

    Implementação js

---
+++ assets/javascript.js
@@ -0,0 +1,8 @@
+
+const button = document.querySelector('.convertButton');
+
+function convertButton () {
+    console.log('Olá mundo! Estou funcionando.')
+}
+
+button.addEventListener('click', convertButton)
