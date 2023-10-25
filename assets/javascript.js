047362b5b4259961602c98f9a6e6c34614e6d8ef
PabloRafael-coder <pabloofael@gmail.com>
2023-10-24 21:08:56 -0300

    Alteration new

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,8 +1,41 @@
+const buttonConvert = document.querySelector('.convert-button');
+const selectMoney = document.querySelector('.select-type-money');
 
-const button = document.querySelector('.convertButton');
 
-function convertButton () {
-    console.log('Olá mundo! Estou funcionando.')
+
+function convertValores() {
+    const inputValue = document.querySelector(".declared-money").value;
+    const valueReal = document.querySelector(".content-value-real");
+    const valueDolar = document.querySelector(".content-value-dolar");
+    const dolar = 4.99;
+    const euro = 5.29
+
+    console.log(selectMoney.value)
+
+
+
+    if (selectMoney.value == 'dolar') {
+        valueDolar.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(inputValue / dolar);
+
+    } else if (selectMoney.value == 'euro') {
+        valueDolar.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: 'currency',
+            currency: 'EUR',
+        }).format(inputValue / euro);
+    }
+
+    valueReal.innerHTML = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    }).format(inputValue);
+
+
+
+
 }
 
-button.addEventListener('click', convertButton)
+buttonConvert.addEventListener("click", convertValores);
+selectMoney.addEventListener('change',)
